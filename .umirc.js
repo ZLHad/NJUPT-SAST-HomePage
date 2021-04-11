895678e5f9b5fdb585ae7f7d8f78ec1dcfeb5911
ZLH_ad <zhangczssx@hotmail.com>
2021-04-11 21:03:23 +0800

    单页面模板over

--- .umirc.js
+++ .umirc.js
@@ -5,7 +5,6 @@ export default {
     routes: [
         {
             path: '/',
-            component: '../layouts/index',
             routes: [
                 { path: '/', component: '../pages/index' }
             ]
